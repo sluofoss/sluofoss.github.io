70342137c9d033e9657aed31503e63856d24a9a5
sluo <41530060+lu0x1a0@users.noreply.github.com>
2025-07-27 22:25:42 +1000

    Quartz sync: Jul 27, 2025, 10:25 PM

--- .foam/graph_export.py
+++ .foam/graph_export.py
@@ -1,42 +1,30 @@
+import numpy as np
 import networkx as nx
 import pathlib
 import re
-
-SAFE_NODE_RE = re.compile(r'^[\w\-]+$')  # Only allow letters, numbers, _, -
-
+files = list(pathlib.Path("./").glob("**/*.md"))
 def _extract_wikilinks(text) -> list[str]:
     return re.findall(r"\[\[(.*?)\]\]", text)
 
 def _extract_hashtags(text) -> list[str]:
+    # hash with more than two chars
     return re.findall(r"#(\w{2,})", text)
 
-def sanitize_node_id(s: str) -> str:
-    # Remove problematic characters and strip whitespace
-    s = re.sub(r'[|<>]', '', s).strip().replace(' ', '_')
-    # Only allow safe characters
-    s = re.sub(r'[^\w\-]', '', s)
-    return s
-
 g = nx.DiGraph()
-files = list(pathlib.Path("./content").glob("**/*.md"))[:20]  # Only first 20 files in ./content
 for file in files:
-    fname_slug = sanitize_node_id(file.stem)
-    if not fname_slug or not SAFE_NODE_RE.match(fname_slug):
-        continue
-    g.add_node(fname_slug, __labels__="File")
-    with open(file, "r", encoding="utf-8") as f:
+    fname_slug = file.stem
+    with open(file, "r") as f:
         text = f.read()
         wikilinks = _extract_wikilinks(text)
         for link in wikilinks:
-            link = sanitize_node_id(link.split("#")[0])
-            if link and SAFE_NODE_RE.match(link):
-                g.add_node(link)
-                g.add_edge(fname_slug, link)
+            link = link.split("#")[0]
+            g.add_edge(fname_slug, link)
+
+        g.add_node(fname_slug, __labels__="File")
+
         hashtags = _extract_hashtags(text)
         for tag in hashtags:
-            tag = sanitize_node_id(tag)
-            if tag and SAFE_NODE_RE.match(tag):
-                g.add_node(tag, __labels__="Tag")
-                g.add_edge(fname_slug, tag, __labels__="Tagged")
+            g.add_edge(fname_slug, tag, __labels__="Tagged")
+            g.add_node(tag, __labels__="Tag")
 
 nx.write_graphml(g, "graph.graphml")
