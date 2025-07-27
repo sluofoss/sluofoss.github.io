d253f67e1d66cf2559ec460c72319f5afcda3c1f
sluo <41530060+lu0x1a0@users.noreply.github.com>
2025-07-27 22:33:51 +1000

    Quartz sync: Jul 27, 2025, 10:33 PM

--- .foam/graph_export.py
+++ .foam/graph_export.py
@@ -2,7 +2,7 @@ import numpy as np
 import networkx as nx
 import pathlib
 import re
-files = list(pathlib.Path("./").glob("**/*.md"))
+files = list(pathlib.Path("./content").glob("**/*.md"))
 def _extract_wikilinks(text) -> list[str]:
     return re.findall(r"\[\[(.*?)\]\]", text)
 
@@ -13,18 +13,18 @@ def _extract_hashtags(text) -> list[str]:
 g = nx.DiGraph()
 for file in files:
     fname_slug = file.stem
-    with open(file, "r") as f:
+    with open(file, "r", encoding="utf-8") as f:
         text = f.read()
         wikilinks = _extract_wikilinks(text)
         for link in wikilinks:
             link = link.split("#")[0]
             g.add_edge(fname_slug, link)
 
-        g.add_node(fname_slug, __labels__="File")
+        g.add_node(fname_slug, __labels__="File", filename=fname_slug)
 
         hashtags = _extract_hashtags(text)
         for tag in hashtags:
             g.add_edge(fname_slug, tag, __labels__="Tagged")
-            g.add_node(tag, __labels__="Tag")
+            g.add_node(tag, __labels__="Tag", filename=fname_slug)
 
 nx.write_graphml(g, "graph.graphml")
